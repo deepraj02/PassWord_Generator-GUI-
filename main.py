0e09338ecfce8b161104ada1eda0e3e5a2377ab5
Deepraj Baidya <63138398+deepraj02@users.noreply.github.com>
2021-04-01 22:26:36 +0530

    Added Themes

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 
+
 # Modules needed
 from tkinter import *
 import tkinter.messagebox as tmsb
 import random
+from ttkthemes import themed_tk as t
+from tkinter import ttk
 
 low = "abcdefghijklmnopqrstuvwxyz"
 upp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 num = "0123456789"
 sym = "[]{}()!@#-$%^&:;*_,.+=""<>~`"
 
-root = Tk()
+root = t.ThemedTk(theme="ubuntu")
 root.wm_iconbitmap("passman.ico")
 root.title("Password-Generator")
-root.geometry("600x460")
+root.geometry("540x370")
 root.configure(bg="#2C3335")
 
 
@@ -21,8 +24,8 @@ f1 = Frame(root, bg="#FF5733", borderwidth=10,relief=SUNKEN,)
 he = Label(f1, text="Password-Generator", font="hack 30 bold").pack(anchor=N)
 f1.pack(padx=2, pady=10)
 
-screen = Entry(root, font="consolas 20",)
-screen.pack(side=LEFT, padx=10, pady=10, ipady=12, fill=BOTH, anchor=N)
+screen = Entry(root, font="consolas 20",width=25)
+screen.pack(side=LEFT, padx=10, pady=10, ipady=7, fill=BOTH, anchor=N)
 
 
 # ^ Functions
@@ -62,34 +65,20 @@ def srt():
 def cle():
     screen.delete(0,"end")
 
-def qui():
-    sure = tmsb.askquestion(
-        "Confirm Exit", "Are you Sure You want to Exit\n???")
-    if sure == "yes":
-        quit()
+
 #! Main body
 
 
 # %Buttons.
+s = ttk.Style().configure('my.TButton', font="hack 14")
 
-b = Button(root, text="Strong", font="Consolas 18",
-           bg="#0DEC76", width=10, command=srt)
-b.pack(padx=10, pady=10, anchor=NE)
-b = Button(root, text="Medium", font="Consolas 18",
-           bg="#0DEC76", width=10, command=med)
-b.pack(padx=10, pady=10, anchor=NE)
 
-b = Button(root, text="Weak", font="Consolas 18",
-           bg="#0DEC76", width=10, command=sh)
-b.pack(padx=10, pady=10, anchor=NE)
+ttk.Button(root, text="Strong", style="my.TButton", width=10, command=srt).pack(padx=10, pady=10, anchor=NE)
+ttk.Button(root, text="Medium", style="my.TButton",width=10, command=med).pack(padx=10, pady=10, anchor=NE)
 
-b = Button(root, text="Clear", font="Consolas 18",
-           bg="#0DEC76", width=10, command=cle)
-b.pack(padx=10, pady=10, anchor=NE)
+ttk.Button(root, text="Weak", style="my.TButton",width=10,command=sh).pack(padx=10, pady=10, anchor=NE)
 
-b = Button(root, text="Exit", font="Consolas 18",
-           bg="#0DEC76", width=10, command=qui)
-b.pack(padx=10, pady=10, anchor=NE)
+ttk.Button(root, text="Clear", style="my.TButton", width=10, command=cle).pack(padx=10, pady=10, anchor=NE)
 
 root.mainloop()
 
